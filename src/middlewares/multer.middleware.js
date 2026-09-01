0bc2b5ad9df7f3f9bb41a5944947e195f91cebfa
khushi <khushi@MacBooks-MacBook-Air.local>
2026-09-01 11:59:19 +0530

    c

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -3,15 +3,16 @@ import multer from "multer";
 import fs from "fs";
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        const dir = "./public/temp";
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-        cb(null, dir);
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
+  destination: function (req, file, cb) {
+    const dir = "./public/temp";
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
     }
+    
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-export const upload=multer({storage:storage})
+export const upload = multer({ storage: storage });
